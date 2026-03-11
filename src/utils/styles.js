67af722af6759753d0c1dea1f00cd0e3172a8dc5
nikaasp <45363258+nikaasp@users.noreply.github.com>
2026-03-11 15:14:57 +0100

    small mod on action bar

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -368,10 +368,11 @@ export const styles = {
     fontSize: "15px",
   },
   bottomActionBar: {
-  marginTop: 16,
-  display: "flex",
-  flexDirection: "column",
-  gap: 10,
+    marginTop: "16px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    paddingBottom: "16px",
   },
   primaryAction: {
     width: "100%",
